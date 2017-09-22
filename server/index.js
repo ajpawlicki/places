a116606a44728eb94f2079c419f5e43dfa497045
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-09-22 00:02:44 -0700

    Create basic server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/../react-client/dist/'));
+
+app.listen(process.env.PORT || 5000, () => console.log('Listening on port 5000!'));
