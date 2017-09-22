72915aead5fae1d9aa3987eef4123671a058d416
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-09-22 02:23:51 -0700

    Add custom marker

--- react-client/src/components/Map.jsx
+++ react-client/src/components/Map.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import logo from '../img/cubs.png';
+
 class Map extends Component {
   constructor(props) {
     super(props);
@@ -11,18 +13,19 @@ class Map extends Component {
     };
 
     this.nearbySearchCallback = this.nearbySearchCallback.bind(this);
+    this.addMarker = this.addMarker.bind(this);
   }
 
   initMap (location) {
     const map = new google.maps.Map(this.refs.map, {
       center: location,
-      zoom: 13
+      zoom: 16
     });
 
     this.setState({
       map: map,
       // infoWindow: new google.maps.InfoWindow()
-      // service: new google.maps.places.PlacesService(map)
+      service: new google.maps.places.PlacesService(map)
     });
 
     const service = new google.maps.places.PlacesService(map);
@@ -35,9 +38,10 @@ class Map extends Component {
   }
 
   nearbySearchCallback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
       for (let i = 0; i < results.length; i++) {
         let place = results[i];
+        this.addMarker(place);
         // createMarker(results[i]);
       }
       
@@ -45,6 +49,35 @@ class Map extends Component {
       console.log('results:', results);
     }
   }
+
+  addMarker(place) {
+    const icon = new google.maps.MarkerImage(
+      logo,
+      null, /* size is determined at runtime */
+      null, /* origin is 0,0 */
+      null, /* anchor is bottom center of the scaled image */
+      new google.maps.Size(50, 50),
+    );
+
+    const marker = new google.maps.Marker({
+      map: this.state.map,
+      icon: icon,
+      position: place.geometry.location
+    });
+  
+    // google.maps.event.addListener(marker, 'click', function() {
+    //   service.getDetails(place, function(result, status) {
+    //     if (status !== google.maps.places.PlacesServiceStatus.OK) {
+    //       console.error(status);
+    //       return;
+    //     }
+    //     infoWindow.setContent(result.name);
+    //     infoWindow.open(map, marker);
+    //   });
+    // });
+
+    return marker;
+  } 
 
   componentDidMount() {
     this.initMap(this.props.currLocation);
