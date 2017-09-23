ba1db53e715ef6d40a4c94abb69302631428474f
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-09-23 01:19:08 -0700

    Add containers to App component for styling

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Map from '../src/components/Map.jsx';
 import Form from '../src/components/Form.jsx';
+import PlacesList from '../src/components/PlacesList.jsx';
 import './style.css';
 
 class App extends Component {
@@ -14,15 +15,15 @@ class App extends Component {
         lat: 41.9484,
         lng: -87.6553
       },
-      searchResults: []
+      places: []
     }
 
-    this.saveSearchResults = this.saveSearchResults.bind(this);
+    this.updatePlaces = this.updatePlaces.bind(this);
     this.setCurrLocation = this.setCurrLocation.bind(this);
   }
 
-  saveSearchResults(results) {
-    this.setState({searchResults: results});
+  updatePlaces(results) {
+    this.setState({places: results});
   }
 
   setCurrLocation(location) {
@@ -31,10 +32,16 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Form setCurrLocation={this.setCurrLocation}/>
-        <Map currLocation={this.state.currLocation}
-          saveSearchResults={this.saveSearchResults} />
+      <div className="container">
+        <div className="left-container">
+          <Map currLocation={this.state.currLocation}
+            updatePlaces={this.updatePlaces} />
+        </div>
+        <div className="right-container">
+          <Form setCurrLocation={this.setCurrLocation} />
+          <h2>Places:</h2>
+          <PlacesList places={this.state.places} />
+        </div>
       </div>
     );
   }
