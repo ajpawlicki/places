5786098b5a73a64896cd8d2a1cf7e9508b2e6000
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-09-23 01:18:29 -0700

    Create PlacesList and Place components

---
+++ react-client/src/components/Place.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Place extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    this.props.place.marker.setAnimation(google.maps.Animation.DROP);
+    
+    // setTimeout(() => {
+    //   this.props.place.marker.setAnimation(null);
+    //  }, 1000);
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}
+        className="place-card">
+        <p><b>{this.props.place.name}</b></p>
+        <p>{this.props.place.vicinity}</p>
+        <p>Type: {this.props.place.types[0].split('_').join(' ')}</p>
+      </div>
+    );
+  }
+}
+
+export default Place;
---
+++ react-client/src/components/PlacesList.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import Place from '../components/Place.jsx';
+
+class PlacesList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="list">
+          {this.props.places.map(place => <Place key={place.id} place={place} />)}
+      </div>
+    );
+  }
+}
+
+export default PlacesList;
