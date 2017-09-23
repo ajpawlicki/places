d66fb8215af7f95512f0b68e0ef1ca3e31d33bc0
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-09-23 01:56:36 -0700

    Abstract state to parent component

--- react-client/src/components/Map.jsx
+++ react-client/src/components/Map.jsx
@@ -8,7 +8,7 @@ class Map extends Component {
     super(props);
 
     this.nearbySearchCallback = this.nearbySearchCallback.bind(this);
-    this.addMarker = this.addMarker.bind(this);
+    // this.addMarker = this.addMarker.bind(this);
   }
 
   initMap (location) {
@@ -19,11 +19,13 @@ class Map extends Component {
     
     const service = new google.maps.places.PlacesService(map);
     
-    this.setState({
-      map: map,
-      // infoWindow: new google.maps.InfoWindow()
-      service: service
-    });
+    this.props.setMapAndService({map, service});
+
+    // this.setState({
+    //   map: map,
+    //   infoWindow: new google.maps.InfoWindow()
+    //   service: service
+    // });
 
     service.nearbySearch({
       location: location,
@@ -36,7 +38,7 @@ class Map extends Component {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       for (let i = 0; i < results.length; i++) {
         let place = results[i];
-        place.marker = this.addMarker(place);
+        // place.marker = this.addMarker(place);
         // createMarker(results[i]);
       }
       
@@ -45,37 +47,37 @@ class Map extends Component {
     }
   }
 
-  addMarker(place) {
-    const icon = new google.maps.MarkerImage(
-      logo,
-      null, /* size is determined at runtime */
-      null, /* origin is 0,0 */
-      null, /* anchor is bottom center of the scaled image */
-      new google.maps.Size(50, 50),
-    );
+  // addMarker(place) {
+  //   const icon = new google.maps.MarkerImage(
+  //     logo,
+  //     null, /* size is determined at runtime */
+  //     null, /* origin is 0,0 */
+  //     null, /* anchor is bottom center of the scaled image */
+  //     new google.maps.Size(50, 50),
+  //   );
 
-    const marker = new google.maps.Marker({
-      map: this.state.map,
-      icon: icon,
-      position: place.geometry.location
-    });
+  //   const marker = new google.maps.Marker({
+  //     map: this.state.map,
+  //     icon: icon,
+  //     position: place.geometry.location
+  //   });
 
-    const infoWindow = new google.maps.InfoWindow();
+  //   const infoWindow = new google.maps.InfoWindow();
 
-    google.maps.event.addListener(marker, 'click', () => {
-      this.state.service.getDetails(place, (result, status) => {
-        if (status !== google.maps.places.PlacesServiceStatus.OK) {
-          console.error(status);
-          return;
-        }
-        infoWindow.setContent(result.name);
-        infoWindow.open(this.state.map, marker);
-        setTimeout(() => infoWindow.close(), 2000);
-      });
-    });
+  //   google.maps.event.addListener(marker, 'click', () => {
+  //     this.state.service.getDetails(place, (result, status) => {
+  //       if (status !== google.maps.places.PlacesServiceStatus.OK) {
+  //         console.error(status);
+  //         return;
+  //       }
+  //       infoWindow.setContent(result.name);
+  //       infoWindow.open(this.state.map, marker);
+  //       setTimeout(() => infoWindow.close(), 2000);
+  //     });
+  //   });
 
-    return marker;
-  } 
+  //   return marker;
+  // }
 
   componentDidMount() {
     this.initMap(this.props.currLocation);
@@ -83,9 +85,9 @@ class Map extends Component {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps !== null && (nextProps.currLocation.lat !== this.props.currLocation.lat || nextProps.currLocation.lng !== this.props.currLocation.lng)) {
-      this.state.map.setCenter(nextProps.currLocation);
+      this.props.map.setCenter(nextProps.currLocation);
       
-      this.state.service.nearbySearch({
+      this.props.service.nearbySearch({
         location: nextProps.currLocation,
         radius: 500,
         type: ['store']
